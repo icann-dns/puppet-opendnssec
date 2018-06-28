a669e01bbd7b557217e3e9acf196220919411e11
Arthur Schutijser <aschutijser@bol.com>
2018-06-28 10:06:59 +0200

    specify the base dir and listening address

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -6,7 +6,7 @@ define opendnssec::zone (
   Optional[Array[String]]        $masters             = [],
   Optional[Array[String]]        $provide_xfrs        = [],
   String                         $order               = '10',
-  Stdlib::Absolutepath           $adapter_base_dir    = $::opendnssec::params::base_dir,
+  Stdlib::Absolutepath           $adapter_base_dir    = $::opendnssec::base_dir,
   Optional[Stdlib::Absolutepath] $adapter_signer_conf = undef,
   Optional[Stdlib::Absolutepath] $adapter_input_file  = undef,
   Optional[Stdlib::Absolutepath] $adapter_output_file = undef,
