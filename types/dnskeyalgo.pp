e25027f0186f9f45d2e6c9c4c8c2fe1bf99ff526
Mauricio Vergara <mauricio.vergara@icann.org>
2021-01-12 10:50:20 -0800

    Adding support for extra algorithms

--- types/dnskeyalgo.pp
+++ types/dnskeyalgo.pp
@@ -3,5 +3,8 @@ type Opendnssec::Dnskeyalgo = Enum[
   'RSASHA256',
   'RSASHA512',
   'RSASHA1-NSEC3-SHA1',
-  'ECDSAP256SHA256'
+  'ECDSAP256SHA256',
+  'ECDSAP384SHA384',
+  'ED25519',
+  'ED448'
 ]
